62806c6c6fb5932905678b1607691a40f176ef47
Mochammad Iqbal <74954973+mochbale@users.noreply.github.com>
2023-06-12 15:53:43 +0700

    Add files via upload

---
+++ streamlite_app.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import streamlit as st
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource
+from bokeh.io import curdoc
+
+# Membaca dataset
+url = 'https://raw.githubusercontent.com/mochbale/Tubes/main/Dataset2.csv'
+data = pd.read_csv(url)
+
+# Membuat data source
+source = ColumnDataSource(data)
+
+# Membuat plot
+plot = figure(x_range=data['Year'].unique(), plot_height=400, plot_width=800, title='Penghasilan Beras di Asia',
+              x_axis_label='Tahun', y_axis_label='Penghasilan (Unit)')
+plot.vbar(x='Year', top='Value', width=0.9, source=source)
+
+# Menambahkan judul plot dan label sumbu
+plot.title.align = 'center'
+plot.title.text_font_size = '18px'
+plot.xaxis.axis_label_text_font_size = '14px'
+plot.yaxis.axis_label_text_font_size = '14px'
+
+# Mengatur tampilan plot
+plot.xgrid.grid_line_color = None
+plot.y_range.start = 0
+
+# Menampilkan plot menggunakan Streamlit
+st.bokeh_chart(plot)
+
+# Menampilkan tabel dataset
+st.write(data)
+
