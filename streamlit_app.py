46a9d6cfb780ae29145d6b27951b8ca8099bacfe
Mochammad Iqbal <74954973+mochbale@users.noreply.github.com>
2023-06-14 14:58:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 from bokeh.io import show
-from bokeh.models import ColumnDataSource, NumeralTickFormatter
+from bokeh.models import ColumnDataSource, NumeralTickFormatter, HoverTool
 from bokeh.plotting import figure
 
 # baca dataset dari tautan GitHub
@@ -14,12 +14,11 @@ source = ColumnDataSource(df_indonesia)
 p = figure(title="Penghasilan Beras di Indonesia", x_axis_label='Tahun', y_axis_label='Penghasilan')
 p.line(x='Year', y='Value', source=source)
 
-# format penghasilan lebih manusiawi dalam ton 
+# atur format tampilan angka pada sumbu y
 p.yaxis[0].formatter = NumeralTickFormatter(format="0,0")
 
 # tambahkan alat HoverTool ke plot
 hover = HoverTool(tooltips=[("Tahun", "@Year"), ("Penghasilan", "@Value{0,0}")])
 p.add_tools(hover)
-
 
 st.bokeh_chart(p)
