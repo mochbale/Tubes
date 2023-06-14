bffb4e1ea6bde4a19c4cf07dd489ea052b8e5b65
Mochammad Iqbal <74954973+mochbale@users.noreply.github.com>
2023-06-14 14:48:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import streamlit as st
 from bokeh.io import show
-from bokeh.models import ColumnDataSource, Select
+from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure
-from bokeh.layouts import column
 
 # baca dataset dari tautan GitHub
 df = pd.read_csv('https://raw.githubusercontent.com/mochbale/Tubes/main/Dataset2.csv')
@@ -15,15 +14,7 @@ source = ColumnDataSource(df_indonesia)
 p = figure(title="Penghasilan Beras di Indonesia", x_axis_label='Tahun', y_axis_label='Penghasilan')
 p.line(x='Year', y='Value', source=source)
 
-select = Select(title="Pilih Tahun:", value="1961", options=[str(year) for year in df_indonesia['Year'].unique()])
+# atur format tampilan angka pada sumbu y
+p.yaxis[0].formatter = NumeralTickFormatter(format="0,0")
 
-
-def update_plot(attr, old, new):
-    selected_year = select.value
-    new_data = df_indonesia[df_indonesia['Year'] == selected_year]
-    source.data = new_data
-
-select.on_change('value', update_plot)
-
-layout = column(select, p)
-st.bokeh_chart(layout)
+st.bokeh_chart(p)
