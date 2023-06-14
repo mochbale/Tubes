40d4229333367f8b0552eeab0596327af01d1501
Mochammad Iqbal <74954973+mochbale@users.noreply.github.com>
2023-06-14 15:12:35 +0700

    Update streamlit_app.py
    laos

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,20 +2,22 @@ import pandas as pd
 import streamlit as st
 from bokeh.io import show
 from bokeh.models import ColumnDataSource, NumeralTickFormatter, HoverTool
+from bokeh.palettes import Category10
 from bokeh.plotting import figure
 
 # baca dataset dari tautan GitHub
 df = pd.read_csv('https://raw.githubusercontent.com/mochbale/Tubes/main/Dataset2.csv')
 
-df_indonesia = df[df['Area'] == 'Indonesia']
-df_malaysia = df[df['Area'] == 'Malaysia']
+# daftar negara di Asia Tenggara
+countries = ['Brunei Darussalam', 'Cambodia', 'Indonesia', "Lao People's Democratic Republic", 'Malaysia', 'Myanmar', 'Philippines', 'Singapore', 'Thailand', 'Viet Nam']
 
-source_indonesia = ColumnDataSource(df_indonesia)
-source_malaysia = ColumnDataSource(df_malaysia)
+p = figure(title="Penghasilan Beras di Asia Tenggara", x_axis_label='Tahun', y_axis_label='Penghasilan (Ton)')
 
-p = figure(title="Penghasilan Beras di Indonesia dan Malaysia", x_axis_label='Tahun', y_axis_label='Penghasilan (Ton)')
-p.line(x='Year', y='Value', source=source_indonesia, legend_label="Indonesia", color="blue")
-p.line(x='Year', y='Value', source=source_malaysia, legend_label="Malaysia", color="red")
+# tambahkan garis untuk setiap negara
+for i, country in enumerate(countries):
+    df_country = df[df['Area'] == country]
+    source_country = ColumnDataSource(df_country)
+    p.line(x='Year', y='Value', source=source_country, legend_label=country, color=Category10[10][i])
 
 # atur format tampilan angka pada sumbu y
 p.yaxis[0].formatter = NumeralTickFormatter(format="0,0")
