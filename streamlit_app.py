f8612588c8a7725d9c91e5d87c6edaeb304625d8
Mochammad Iqbal <74954973+mochbale@users.noreply.github.com>
2023-06-16 14:18:59 +0700

    Update streamlit_app.py
    reset

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,15 +11,26 @@ df = pd.read_csv('https://raw.githubusercontent.com/mochbale/Tubes/main/Dataset2
 countries = ['Brunei Darussalam', 'Cambodia', 'Indonesia', "Lao People's Democratic Republic", 'Malaysia', 'Myanmar', 'Philippines', 'Singapore', 'Thailand', 'Viet Nam']
 
 # tambahkan widget selectbox untuk memilih negara
-selected_country = st.selectbox('Pilih Negara:', countries)
+selected_country = st.selectbox('Pilih Negara:', ['Semua Negara'] + countries)
 
-# filter DataFrame untuk hanya menyertakan data untuk negara yang dipilih
-df_country = df[df['Area'] == selected_country]
-source_country = ColumnDataSource(df_country)
+# tambahkan tombol reset
+if st.button('Reset'):
+    selected_country = 'Semua Negara'
 
-# buat plot baru untuk negara yang dipilih dengan lebar dan tinggi yang ditentukan
-p = figure(title=f"Penghasilan Beras di {selected_country} 1961~2021", x_axis_label='Tahun', y_axis_label='Penghasilan (Ton)', plot_width=900, plot_height=600)
-p.line(x='Year', y='Value', source=source_country, legend_label=selected_country, color=Category10[10][0])
+# buat plot baru dengan lebar dan tinggi yang ditentukan
+p = figure(title="Penghasilan Beras di Asia Tenggara 1961~2021", x_axis_label='Tahun', y_axis_label='Penghasilan (Ton)', plot_width=920, plot_height=680)
+
+if selected_country == 'Semua Negara':
+    # tampilkan semua negara
+    for i, country in enumerate(countries):
+        df_country = df[df['Area'] == country]
+        source_country = ColumnDataSource(df_country)
+        p.line(x='Year', y='Value', source=source_country, legend_label=country, color=Category10[10][i])
+else:
+    # tampilkan hanya negara yang dipilih
+    df_country = df[df['Area'] == selected_country]
+    source_country = ColumnDataSource(df_country)
+    p.line(x='Year', y='Value', source=source_country, legend_label=selected_country, color=Category10[10][0])
 
 # atur format tampilan angka pada sumbu y
 p.yaxis[0].formatter = NumeralTickFormatter(format="0,0")
