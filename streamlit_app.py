b0dc6f59dbaf8195619397a5611b13658559f6b0
Mochammad Iqbal <74954973+mochbale@users.noreply.github.com>
2023-06-16 14:08:00 +0700

    Update streamlit_app.py
    penamabahan selectbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import streamlit as st
-from bokeh.io import show
 from bokeh.models import ColumnDataSource, NumeralTickFormatter, HoverTool
 from bokeh.palettes import Category10
 from bokeh.plotting import figure
@@ -11,13 +10,16 @@ df = pd.read_csv('https://raw.githubusercontent.com/mochbale/Tubes/main/Dataset2
 # daftar negara di Asia Tenggara
 countries = ['Brunei Darussalam', 'Cambodia', 'Indonesia', "Lao People's Democratic Republic", 'Malaysia', 'Myanmar', 'Philippines', 'Singapore', 'Thailand', 'Viet Nam']
 
-p = figure(title="Penghasilan Beras di Asia Tenggara 1961~2021", x_axis_label='Tahun', y_axis_label='Penghasilan (Ton)')
+# tambahkan widget selectbox untuk memilih negara
+selected_country = st.selectbox('Pilih Negara:', countries)
 
-# tambahkan garis untuk setiap negara
-for i, country in enumerate(countries):
-    df_country = df[df['Area'] == country]
-    source_country = ColumnDataSource(df_country)
-    p.line(x='Year', y='Value', source=source_country, legend_label=country, color=Category10[10][i])
+# filter DataFrame untuk hanya menyertakan data untuk negara yang dipilih
+df_country = df[df['Area'] == selected_country]
+source_country = ColumnDataSource(df_country)
+
+# buat plot baru untuk negara yang dipilih
+p = figure(title=f"Penghasilan Beras di {selected_country} 1961~2021", x_axis_label='Tahun', y_axis_label='Penghasilan (Ton)')
+p.line(x='Year', y='Value', source=source_country, legend_label=selected_country, color=Category10[10][0])
 
 # atur format tampilan angka pada sumbu y
 p.yaxis[0].formatter = NumeralTickFormatter(format="0,0")
