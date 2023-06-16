6c4044e7f39a7c6d3be4f3f40659d7dec0f0f304
Mochammad Iqbal <74954973+mochbale@users.noreply.github.com>
2023-06-16 14:28:58 +0700

    Update streamlit_app.py
    testing slider testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,38 +4,46 @@ from bokeh.models import ColumnDataSource, NumeralTickFormatter, HoverTool
 from bokeh.palettes import Category10
 from bokeh.plotting import figure
 
-# Membaca dataset dari tautan GitHub
+# baca dataset dari tautan GitHub
 df = pd.read_csv('https://raw.githubusercontent.com/mochbale/Tubes/main/Dataset2.csv')
 
-# Daftar negara di Asia Tenggara
+# daftar negara di Asia Tenggara
 countries = ['Brunei Darussalam', 'Cambodia', 'Indonesia', "Lao People's Democratic Republic", 'Malaysia', 'Myanmar', 'Philippines', 'Singapore', 'Thailand', 'Viet Nam']
 
-# Menambahkan widget selectbox untuk memilih negara
+# tambahkan widget selectbox untuk memilih negara
 selected_country = st.selectbox('Pilih Negara:', ['Semua Negara'] + countries)
 
-# Membuat plot baru dengan lebar dan tinggi yang ditentukan
-p = figure(title="Penghasilan Beras di Asia Tenggara 1961~2021", x_axis_label='Tahun', y_axis_label='Penghasilan (Ton)', plot_width=920, plot_height=680)
+# tambahkan widget slider untuk memilih rentang tahun
+min_year = int(df['Year'].min())
+max_year = int(df['Year'].max())
+start_year, end_year = st.slider('Pilih Rentang Tahun:', min_year, max_year, (min_year, max_year))
+
+# filter DataFrame untuk hanya menyertakan data untuk rentang tahun yang dipilih
+df = df[(df['Year'] >= start_year) & (df['Year'] <= end_year)]
+
+# buat plot baru dengan lebar dan tinggi yang ditentukan
+p = figure(title="Penghasilan Beras di Asia Tenggara 1961~2021", x_axis_label='Tahun', y_axis_label='Penghasilan (Ton)', plot_width=320, plot_height=680)
 
 if selected_country == 'Semua Negara':
-    # Menampilkan semua negara
+    # tampilkan semua negara
     for i, country in enumerate(countries):
         df_country = df[df['Area'] == country]
         source_country = ColumnDataSource(df_country)
         p.line(x='Year', y='Value', source=source_country, legend_label=country, color=Category10[10][i])
 else:
-    # Menampilkan hanya negara yang dipilih
+    # tampilkan hanya negara yang dipilih
     df_country = df[df['Area'] == selected_country]
     source_country = ColumnDataSource(df_country)
     p.line(x='Year', y='Value', source=source_country, legend_label=selected_country, color=Category10[10][0])
 
-# Mengatur format tampilan angka pada sumbu y
+# atur format tampilan angka pada sumbu y
 p.yaxis[0].formatter = NumeralTickFormatter(format="0,0")
 
-# Menambahkan alat HoverTool ke plot
+# tambahkan alat HoverTool ke plot
 hover = HoverTool(tooltips=[("Negara", "@Area"), ("Tahun", "@Year"), ("Penghasilan", "@Value{0,0} Ton")])
 p.add_tools(hover)
 
-# Menambahkan spasi antara judul dan plot
+# tambahkan spasi antara judul dan plot
 p.title.align = 'center'
 p.title.text_font_size = '16pt'
 p.title.offset = 20
